f4ddfaa7df5d0546d348c5fec3054d7dcf25502a
Максат Тезекбаев <maksat013@gmail.com>
2017-10-26 13:05:29 +0600

    dsfdsfd

--- assistant_telegram/admin.py
+++ assistant_telegram/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
-# from assistant_telegram.models import Context_chat
-# from assistant_telegram.models import Entity
-# from assistant_telegram.models import Undefined_msg
-# from assistant_telegram.models import Story
-# from assistant_telegram.models import Story_entity
-# from assistant_telegram.models import Log
+from assistant_telegram.models import Intent,Story_msg,Story_action,Story_entity
+from assistant_telegram.models import Chat_id,Context_chat,Entity,Log
+
 # Register your models here.
-# admin.site.register(Context_chat)
-# admin.site.register(Entity)
-# admin.site.register(Undefined_msg)
-# admin.site.register(Story)
-# admin.site.register(Story_entity)
-# admin.site.register(Log_msg)
+admin.site.register(Intent)
+admin.site.register(Story_msg)
+admin.site.register(Story_action)
+admin.site.register(Story_entity)
+admin.site.register(Chat_id)
+admin.site.register(Context_chat)
+admin.site.register(Entity)
+admin.site.register(Log)
--- assistant_telegram/models.py
+++ assistant_telegram/models.py
@@ -2,9 +2,33 @@ from django.db import models
 
 
 # Create your models here.
+
+class Intent(models.Model):
+	name=models.CharField(primary_key=True,max_length=60)
+
+	def __str__(self):
+		str1=str(self.name)
+		return str1
+
+class Chat_id(models.Model):
+	idnumber=models.BigIntegerField(primary_key=True)
+
+	def __str__(self):
+		str1=str(self.idnumber)
+		return str1
+
+
+
 class Context_chat(models.Model):
-	chat_id=models.BigIntegerField(primary_key=True)
-	intent=models.CharField(max_length=60)
+	chat_id=models.OneToOneField(
+        Chat_id,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+	intent=models.OneToOneField(
+        Intent,
+        on_delete=models.CASCADE
+    )
 	validated_action=models.BooleanField()
 	prev_msg_from_bot=models.CharField(max_length=200,default=" ")
 
@@ -23,17 +47,26 @@ class Entity(models.Model):
 	def __str__(self):
 		return str(self.name)+" "+str(self.value)+" " + str(self.confidence)+" "+str(self.chat.chat_id)
 
+
 class Story_msg(models.Model):
-	intent=models.CharField(primary_key=True,max_length=60)
+	intent=models.OneToOneField(
+        Intent,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
 	msg_example=models.TextField(default=" ")
-	anwer=models.TextField(default=" ")
+	answer=models.TextField(default=" ")
 
 	def __str__(self):
 		str1=str(self.intent)+": " + str(self.answer)
 		return str1
 
 class Story_action(models.Model):
-	intent=models.CharField(primary_key=True,max_length=60)
+	intent=models.OneToOneField(
+        Intent,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
 	validation_option=models.BooleanField(default="False")
 	action_name=models.CharField(max_length=60,default=" ")
 	action_answer=models.TextField(default=" ")
@@ -53,13 +86,12 @@ class Story_entity(models.Model):
 
 class Log(models.Model):
 	msg=models.TextField(max_length=200, default=" ")
-	intent=models.CharField(max_length=200,default=" ")
+	intent=models.ForeignKey(Intent,on_delete=models.SET_NULL,null=True)
+	date_in_s=models.BigIntegerField()
+	chat_id=models.ForeignKey(Chat_id,on_delete=models.SET_NULL,null=True)
+	viewed=models.BooleanField(default="False")
+
+
 	def __str__(self):
-		return str(self.msg)
-
-
-class Undefined_msg(models.Model):
-	msg=models.CharField(max_length=200)
-	def __str__(self):
-		return str(self.msg)
-
+		str1=str(self.msg)+" | "+str(self.intent)+" | "+str(self.chat_id.idnumber)+" | "+str(self.viewed)
+		return str1
