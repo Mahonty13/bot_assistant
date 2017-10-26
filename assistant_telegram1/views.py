87359df2589ba77aec7134846cbf11b1c625366f
Максат Тезекбаев <maksat013@gmail.com>
2017-10-26 16:02:37 +0600

    dsfdfdsf

--- assistant_telegram1/views.py
+++ assistant_telegram1/views.py
@@ -10,170 +10,161 @@ from assistant_telegram1.models import *
 client = Wit('TCLMX5YEBCRG5TO2TLW3VJCOGFOWMOJW')
 
 @csrf_exempt
-def assistant_body(chat_id,message):
-    # #Получаем значение сообщения
-    # resps=client.message(message)
-    # #создаем list всех существующих интентов в stories
-    # story_intents=[]
-    # for story in Story.objects.all():
-    # 	story_intents.append(story.intent)
+def assistant_body(chat_id,date_in_s,message):
+    #Сохраняем чат id в БД
+    if not Chat_id.objects.filter(idnumber=chat_id).exists():
+        chat_idmodel=Chat_id(idnumber=int(chat_id))
+        chat_idmodel.save()
+    current_chat_id=Chat_id.objects.get(idnumber=chat_id)
 
-    # print(str(chat_id) + "   get_msg:    "+str(resps))
-    # resps=resps['entities']
-    # msg_entities={}
-    # #create dictionary with entities in message
-    # for ent in resps:
-    #     msg_entities[ent]=resps[ent][0]
-    # print(str(chat_id) + "  msg entities:    "+str(msg_entities))
+    #Получаем значение сообщения
+    resps=client.message(message)
+    # #создаем list всех существующих интентов в stories
+    story_intents=[]
+    for intent in Intent.objects.all():
+        story_intents.append(intent.name)
+
+    print(str(chat_id) + "   get_msg:    "+str(resps))
+    resps=resps['entities']
+    msg_entities={}
+    # create dictionary with entities in message
+    for ent in resps:
+        msg_entities[ent]=resps[ent][0]
+    print(str(chat_id) + "  msg entities:    "+str(msg_entities))
+
+
+    #сохраняем логи
 
     # #if the First chat with the user
-    # if not Context_chat.objects.filter( chat_id = chat_id).exists():
-    #     #identify intent
-    #     intent="general"
-    #     if 'intent' in msg_entities:
-    #         if msg_entities['intent']['confidence']>0.8:
-    #             intent = msg_entities['intent']['value']
-    #             logmsg=Log_msg(msg=message,intent=intent)
-    #             logmsg.save()
-    #     else:
-    #         und_msg=Undefined_msg(msg=message)
-    #         und_msg.save()
-    #         intent = 'general'
-    #     # if story for this intent does not exist
-    #     if intent not in story_intents:
-    #         und_msg=Undefined_msg(msg=message)
-    #         und_msg.save()
-    #         intent='general'
-    #     #save new context_chat for the new user with intent
-    #     new_chat = Context_chat(chat_id= chat_id, intent = intent, validated_action = False,prev_msg_from_bot=" ")
-    #     new_chat.save()
-    # else:
-    #     current_chat=Context_chat.objects.get(chat_id = chat_id)
-    #     if 'intent' in msg_entities:
-    #     	#Если есть новый интент, то удаляем context и начинаем обрабатывать новый запрос
-    #         if msg_entities['intent']['confidence']>0.8:
-    #             intent=msg_entities['intent']['value']
-    #             logmsg=Log_msg(msg=message,intent=intent)
-    #             logmsg.save()
-    #             current_chat=Context_chat.objects.get(chat_id=chat_id)
-    #             current_chat.delete()
+    if not hasattr(current_chat_id,"context_chat"):
+        #identify intent
+        intent="undefined"
+        if 'intent' in msg_entities:
+            if msg_entities['intent']['confidence']>0.8:
+                intent = msg_entities['intent']['value']
+        # if story for this intent does not exist
+        if intent not in story_intents:
+            intent='undefined'
+        #save new context_chat for the new user with intent
+        current_intent=Intent.objects.get(name=intent)
+        new_chat = Context_chat(chat_id= current_chat_id, intent = current_intent, validated_action = False,prev_msg_from_bot=" ")
+        new_chat.save()
+    else:
+        if 'intent' in msg_entities:
+            #Если есть новый интент, то удаляем context и начинаем обрабатывать новый запрос
+            if msg_entities['intent']['confidence']>0.8:
+                intent=msg_entities['intent']['value']
+                current_chat.delete()
     #             #Если интента нет в Stories, то сохраняем его в undefined_msg, и делаем его general
-    #             if intent not in story_intents:
-    #                 und_msg=Undefined_msg(msg=message)
-    #                 und_msg.save()
-    #                 intent='general'
-    #             new_chat = Context_chat(chat_id= chat_id, intent = intent, validated_action = False,prev_msg_from_bot=" ")
-    #             new_chat.save()
+                if intent not in story_intents:
+                    intent='undefined'
+                current_intent=Intent.objects.get(name=intent)    
+                new_chat = Context_chat(chat_id= current_chat_id, intent = current_intent, validated_action = False,prev_msg_from_bot=" ")
+                new_chat.save()
 
-    # #Подтверждения нужны, чтобы не прошли ненужные запросы
-    # if "confirmation" in msg_entities:
+    current_chat=current_chat_id.context_chat
+    #Подтверждения нужны, чтобы не прошли ненужные запросы
+    if "confirmation" in msg_entities:
     # 	#Если нет, запускается интент cancel
-    #     if msg_entities['confirmation']['value']=="Нет":
-    #         current_chat=Context_chat.objects.get(chat_id=chat_id)
-    #         current_chat.validated_action=False
-    #         current_chat.intent='cancel'
-    #         current_chat.save()
-    #     else:
-    #         if msg_entities['confirmation']['value']=="Да":
-    #             current_chat=Context_chat.objects.get(chat_id=chat_id)
-    #             current_chat.validated_action=True
-    #             current_chat.save()
+        if msg_entities['confirmation']['value'].lowercase=="нет":
+            if hasattr(current_chat.intent,"story_action"):
+                if current_chat.intent.story_action.validation_option==True:
+                    current_chat.validated_action=False
+                    current_chat.intent=Intent.objects.get(name='cancel')
+                    current_chat.save()      
+        else:
+            if msg_entities['confirmation']['value'].lowercase=="да":
+                current_chat.validated_action=True
+                current_chat.save()
 
-    # current_chat=Context_chat.objects.get(chat_id=chat_id)
-
+    current_chat=current_chat_id.context_chat
     # #if there was a question from bot for particular entity:
-    # if not current_chat.prev_msg_from_bot==" ":
+    if not current_chat.prev_msg_from_bot==" ":
     #     #addition of prev_msg_from_bot to entire msg
-    #     message=current_chat.prev_msg_from_bot+" " + message
-    #     print("message with prev_msg_from_bot: " + str(message))
-    #     resps=client.message(message)
-    #     print(str(chat_id) + "   get_new_msg_with_prev_bot_msg:    "+str(resps))
-    #     resps=resps['entities']
-    #     msg_entities={}
+        message=current_chat.prev_msg_from_bot+" " + message
+        print("message with prev_msg_from_bot: " + str(message))
+        resps=client.message(message)
+        print(str(chat_id) + "   get_new_msg_with_prev_bot_msg:    "+str(resps))
+        resps=resps['entities']
+        msg_entities={}
     #     #create dictionary with entities in new message with prev_msg_from_bot
-    #     for ent in resps:
-    #         msg_entities[ent]=resps[ent][0]
-    #     print(str(chat_id) + "  new msg entities with prev_msg_from_bot:    "+str(msg_entities))
+        for ent in resps:
+            msg_entities[ent]=resps[ent][0]
+        print(str(chat_id) + "  new msg entities with prev_msg_from_bot:    "+str(msg_entities))
 
 
-    # current_chat=Context_chat.objects.get(chat_id=chat_id)
-    # current_story=Story.objects.get(intent=current_chat.intent)
-    # necessary_story_entities=[]
-    # for story_ent in current_story.story_entity_set.all():
-    # 	if story_ent.name!=" ":  
-    #     	necessary_story_entities.append(story_ent.name)
-    # print(str(chat_id) + "   necessary entities for story: "+str(necessary_story_entities))
-    # #add entities to the context
-    # for entity in msg_entities.keys():
-    #     if 'intent' not in entity and msg_entities[entity]['confidence']>0.65:
-    #         #add only entities required for story
-    #         if entity in necessary_story_entities:
-    #             #if entity in new msg already exist, rewrite entity
-    #                 if Entity.objects.filter(chat=current_chat,name=entity).exists():
-    #                     current_entity=Entity.objects.get(chat=current_chat,name=entity)
-    #                     current_entity.value=msg_entities[entity]['value']
-    #                     current_entity.save()
-    #                 else:
-    #                     #save new entities in context
-    #                     new_entity = Entity(name=entity, chat=current_chat, value = msg_entities[entity]['value'],confidence=msg_entities[entity]['confidence'])
-    #                     new_entity.save()
-
-    # print("current_chat:   " + str(current_chat))
-    # #create dictionary context with saved entities and its values
-    # context_entities_list={}
-    # for entity in current_chat.entity_set.all():
-    #     context_entities_list[entity.name]=entity.value
+    necessary_story_entities=[]
+    if hasattr(current_chat.intent,"story_action"):
+        current_story=current_chat.intent.story_action
+        for story_ent in current_story.story_entity_set.all():
+            if story_ent.name!=" ":  
+                necessary_story_entities.append(story_ent.name)
+        print(str(chat_id) + "   necessary entities for story: "+str(necessary_story_entities))
+        #add entities to the context
+        for entity in msg_entities.keys():
+            if 'intent' not in entity and msg_entities[entity]['confidence']>0.65:
+                #add only entities required for story
+                if entity in necessary_story_entities:
+                    # if entity in new msg already exist, rewrite entity
+                    if Entity.objects.filter(chat=current_chat,name=entity).exists():
+                        current_entity=Entity.objects.get(chat=current_chat,name=entity)
+                        current_entity.value=msg_entities[entity]['value']
+                        current_entity.save()
+                    else:
+                        # save new entities in context
+                        new_entity = Entity(name=entity, chat=current_chat, value = msg_entities[entity]['value'],confidence=msg_entities[entity]['confidence'])
+                        new_entity.save()
 
 
-    # print(str(chat_id) + "  context:     " + str(context_entities_list))
-    # #if all required entities are in context, run actions or send msg
-    # if all(ent in context_entities_list for ent in necessary_story_entities):
-    #     current_chat=Context_chat.objects.get(chat_id=chat_id)
-    #     command=Story.objects.get(intent=current_chat.intent)
-    #     if command.story_type==Story.msg:
-    #         print(command.message)
-    #         current_chat.delete()
-    #         return {"text":command.message}
-    #     else:
-    #         if command.story_type==Story.act:
-    #             #if user validated request
-    #             if current_chat.validated_action==True and command.validation_option:
-    #                 current_chat=Context_chat.objects.get(chat_id=chat_id)
-    #                 entities ={}
-    #                 for entity in current_chat.entity_set.all():
-    #                     entities[entity.name]=entity.value
-    #                 current_chat.delete()
-    #                 return {"action": command.action_name,"answer":command.answer,"entities":entities}
-    #             else:
-    #                 #if user has not validated request yet
-    #                 if current_chat.validated_action==False and command.validation_option:
-    #                     #create context entities list with entity values for parameters
-    #                     context_entities_list=[]
-    #                     for entity in current_chat.entity_set.all():
-    #                         context_entities_list.append(entity.value)
-    #                     parameters=', '.join(''.join(v) for v in context_entities_list)
-    #                     print(parameters)
-    #                     answer="Вы хотите " + command.name+" с параметрами " + parameters+"? Скажите да для подтверждения или нет для отмены запроса."
-    #                     current_chat.prev_msg_from_bot=" "
-    #                     current_chat.save()
-    #                     return {"text": answer}
-    #         else:
-    #             #In case of an apocalypse
-    #             print("Команды не найдено")
-    #             current_chat.delete()
-    #             return {"text": "Команды не найдено"}
-                
-    # else:
-    #     #if something is missing
-    #     for ent in necessary_story_entities:
-    #         if ent not in context_entities_list:
-    #             #send question of missing entity
-    #             current_chat=Context_chat.objects.get(chat_id=chat_id)
-    #             current_story=Story.objects.get(intent=current_chat.intent)
-    #             entity_question=Story_entity.objects.get(intent=current_story,name=ent).question
-    #             print(entity_question)
-    #             current_chat.prev_msg_from_bot=entity_question
-    #             current_chat.save()
-    #             print("current_chat in the end "+str(current_chat))
-    #             return {"text": entity_question}
+
+
+    print("current_chat:   " + str(current_chat))
+    #create dictionary context with saved entities and its values
+    context_entities_list={}
+    for entity in current_chat.entity_set.all():
+        context_entities_list[entity.name]=entity.value
+    print(str(chat_id) + "  context:     " + str(context_entities_list))
+
+    if hasattr(current_chat.intent,'story_msg'):
+        answer=current_chat.intent.story_msg.answer
+        print(answer)
+        current_chat.delete()
+        return {"text":answer}
+    else:
+        if hasattr(current_chat.intent,'story_action'):
+            if all(ent in context_entities_list for ent in necessary_story_entities):
+                if current_chat.intent.story_action.validation_option:
+                    if current_chat.validated_action:
+                        entities ={}
+                        for entity in current_chat.entity_set.all():
+                            entities[entity.name]=entity.value
+                        current_chat.delete()
+                        return {"action": current_chat.intent.story_action.action_name,"answer":current_chat.intent.story_action.action_answer,"entities":entities}
+                    else:
+                        context_entities_list=[]
+                        for entity in current_chat.entity_set.all():
+                            context_entities_list.append(entity.value)
+                        parameters=', '.join(''.join(v) for v in context_entities_list)
+                        print(parameters)
+                        answer="Вы хотите " + current_chat.intent.story_action.transcript+" с параметрами " + parameters+"? Скажите да для подтверждения или нет для отмены запроса."
+                        current_chat.prev_msg_from_bot=answer
+                        current_chat.save()
+                        return {"text": answer}
+                else:
+                #In case of an apocalypse
+                print("Команды не найдено")
+                current_chat.delete()
+                return {"text": "Команды не найдено"}                
+            else:
+        	#if something is missing
+            for ent in necessary_story_entities:
+                if ent not in context_entities_list:
+                #send question of missing entity
+                    current_story=current_chat.intent.story_action
+                    entity_question=Story_entity.objects.get(intent=current_story,name=ent).question
+                    print(entity_question)
+                    current_chat.prev_msg_from_bot=entity_question
+                    current_chat.save()
+                    print("current_chat in the end "+str(current_chat))
+                    return {"text": entity_question}
     return HttpResponse(status=200)
--- handler/views.py
+++ handler/views.py
@@ -10,7 +10,7 @@ from assistant_telegram1.views import assistant_body
 
 # Create your views here.
 token_telegram ='465116134:AAEBZ9TEMk1b2kx8uySx-GZllAcHIy5I4js'
-client = Wit('TCLMX5YEBCRG5TO2TLW3VJCOGFOWMOJW')
+client = Wit('OHNOVUONWOIA3NPRI2ER4CNMDBAWAJBX')
 
 def handler_commands(data):
     if data['text'] == '/start':
@@ -67,8 +67,35 @@ def send_tg_voice(chat_id, text):
     # r = requests.post('https://api.telegram.org/bot'+token_telegram+ "/sendVoice", params=payload, files=file)
     g = requests.post('https://api.telegram.org/bot'+token_telegram+ "/sendVoice", params=payload, files=file1)
 
-actions_story={
 
+
+
+##ADD HERE##
+
+def template_action(entities):
+
+    #add your actions
+
+    #create dictionary with name of variables(key) and values of variables required for your answer
+    answer_vars={}
+    answer_vars['variable_name']='value'
+    return answer_vars
+
+
+def currency_exchange(entities):
+
+    #add your actions
+
+    #create dictionary with name of variables(key) and values of variables required for your answer
+    answer_vars={}
+    answer_vars['currencyFrom']='1 доллар'
+    answer_vars['currencyTo']='340 тенге'
+
+    return answer_vars
+
+
+actions_story={
+"currency_exchange":currency_exchange
 }
 
 
@@ -79,70 +106,71 @@ def bot_new(request):
     #в request все данные из сообщения
     data=json.loads(request.body)
     print("data      " + str(data))
-  #   chat_id=data['message']['chat']['id']
-  #   #text handler
-  #   if 'text' in data['message']:
-  #       #if commands / as start,help
-  #       if data['message']['text'][0] == '/':
-  #           #command handler
-  #           handler_commands(data['message'])
-  #           return HttpResponse(status=200)
-  #       else:
-  #           #текст сообщения, которое мы отправим app assistant 
-  #           message=data['message']['text']
-  #   else:
-  #   	pass
-		# #if voice message
-  #       # if 'voice' in data['message']:
-  #       #     message=" "
-  #       #     #Если пустое аудио, то сообщение останется пустым и не отвеченным
-  #       #     if data['message']['voice']['file_size']<2500:
-  #       #         return HttpResponse(status=200)
-  #       #     #Находим file_path, чтобы скачать аудио
-  #       #     r = json.loads(str(requests.get('http://api.telegram.org/bot'+token_telegram+'/getFile', params={'file_id':data['message']['voice']['file_id']}).text))
-  #       #     #Скачиваем аудио по file_path
-  #       #     g=requests.get('http://api.telegram.org/file/bot'+token_telegram+'/'+str(r['result']['file_path']), stream=True)
-  #       #     print("Request from telegram audio file_path" + str(g))
-  #       #     #Записываем аудио в файл voice.ogg, который хранится в главной папке django проекта
-  #       #     with open("voice.ogg", "wb") as o:
-  #       #         o.write(g.content)
-  #       #     #convert ogg to wav
-  #       #     AudioSegment.from_ogg("voice.ogg").export("voice.mp3", format="mp3")
-  #       #     #recognition
+    chat_id=data['message']['chat']['id']
+    date_in_s=int(data['message']['date'])
+    #text handler
+    if 'text' in data['message']:
+        #if commands / as start,help
+        if data['message']['text'][0] == '/':
+            #command handler
+            handler_commands(data['message'])
+            return HttpResponse(status=200)
+        else:
+            #текст сообщения, которое мы отправим app assistant 
+            message=data['message']['text']
+    else:
+    	pass
+		#if voice message
+        # if 'voice' in data['message']:
+        #     message=" "
+        #     #Если пустое аудио, то сообщение останется пустым и не отвеченным
+        #     if data['message']['voice']['file_size']<2500:
+        #         return HttpResponse(status=200)
+        #     #Находим file_path, чтобы скачать аудио
+        #     r = json.loads(str(requests.get('http://api.telegram.org/bot'+token_telegram+'/getFile', params={'file_id':data['message']['voice']['file_id']}).text))
+        #     #Скачиваем аудио по file_path
+        #     g=requests.get('http://api.telegram.org/file/bot'+token_telegram+'/'+str(r['result']['file_path']), stream=True)
+        #     print("Request from telegram audio file_path" + str(g))
+        #     #Записываем аудио в файл voice.ogg, который хранится в главной папке django проекта
+        #     with open("voice.ogg", "wb") as o:
+        #         o.write(g.content)
+        #     #convert ogg to wav
+        #     AudioSegment.from_ogg("voice.ogg").export("voice.mp3", format="mp3")
+        #     #recognition
             
-  #       #     with open('voice.mp3', 'rb') as f:
-  #       #         try:
-  #       #             resp = client.speech(f, None, {'Content-Type': 'audio/mpeg3'})
-  #       #             print('Yay, got Wit.ai response: ' + str(resp))
-  #       #             if "_text" in resp:
-  #       #                 #текст сообщения, которое мы отправим app assistant 
-  #       #                 message = resp['_text']
-  #       #         except:
-  #       #             pass
-  #       #     print(message)
-  #       #     #Если не смог распознать, то меняем None на " ", чтобы не возникла ошибка
-  #       #     if message==None:
-  #       #         message=" "
-  #           # print(message)
-  #   #Отправляем сообщение app assistant, app assistant отправляет нам ответ в json формате
-  #   #структура answer {"text": "текст ответа инф вопроса"}
-  #   #структура answer для action {"action": "name_of_function", "answer": "Ваш почтовый индекс - {post_index}","entities": entities}
-  #   #entities - это dictionary, где key - name of entity, нужный для осуществления конкретного action, value - его value
-  #   answer=assistant_body(chat_id,message)
-  #   if 'text' in answer:
-  #       send(chat_id,answer['text'])
-  #   if "action" in answer:
-  #       try:
-  #           #тут вызывается action, assistant возвращает название функции, и функция вызывается через actions_story
-  #           #answer vars это variables нужный для ответа action
-  #           answer_vars=actions_story[answer['action']](chat_id,answer['entities'])
-  #           print("answer_vars body :" + str(answer_vars))
-  #           #формируется готовый ответ
-  #           answer_to_user=answer['answer'].format(**answer_vars)
-  #           print("Answer:   " +  answer_to_user)
-  #           send(chat_id, answer_to_user)
-  #       except:
-  #           send(chat_id, "Ваш запрос не удался")
+        #     with open('voice.mp3', 'rb') as f:
+        #         try:
+        #             resp = client.speech(f, None, {'Content-Type': 'audio/mpeg3'})
+        #             print('Yay, got Wit.ai response: ' + str(resp))
+        #             if "_text" in resp:
+        #                 #текст сообщения, которое мы отправим app assistant 
+        #                 message = resp['_text']
+        #         except:
+        #             pass
+        #     print(message)
+        #     #Если не смог распознать, то меняем None на " ", чтобы не возникла ошибка
+        #     if message==None:
+        #         message=" "
+            # print(message)
+    #Отправляем сообщение app assistant, app assistant отправляет нам ответ в json формате
+    #структура answer {"text": "текст ответа инф вопроса"}
+    #структура answer для action {"action": "name_of_function", "answer": "Ваш почтовый индекс - {post_index}","entities": entities}
+    #entities - это dictionary, где key - name of entity, нужный для осуществления конкретного action, value - его value
+    answer=assistant_body(chat_id,date_in_s,message)
+    if 'text' in answer:
+        send(chat_id,answer['text'])
+    if "action" in answer:
+        try:
+            #тут вызывается action, assistant возвращает название функции, и функция вызывается через actions_story
+            #answer vars это variables нужный для ответа action
+            answer_vars=actions_story[answer['action']](answer['entities'])
+            print("answer_vars body :" + str(answer_vars))
+            #формируется готовый ответ
+            answer_to_user=answer['answer'].format(**answer_vars)
+            print("Answer:   " +  answer_to_user)
+            send(chat_id, answer_to_user)
+        except:
+            send(chat_id, "Ваш запрос не удался")
             
 
     return HttpResponse(status=200)
