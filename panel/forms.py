233a0727e6b46413393a5a7704cf305712bbff50
Максат Тезекбаев <maksat013@gmail.com>
2017-10-29 02:22:48 +0600

    dfsfd

--- panel/forms.py
+++ panel/forms.py
@@ -16,22 +16,37 @@ class Story_msgForm(forms.ModelForm):
 
     class Meta:
         model = Story_msg
+
+        widgets = { 
+        'msg_example': forms.Textarea(attrs={'rows': 2, 'cols': 40}),
+        'answer': forms.Textarea(attrs={'rows': 2, 'cols': 40}),
+        }
+
+
         labels={
         'msg_example':'Пример сообщения',
         'answer':'ответ'
         }
         fields = ('msg_example', 'answer')
 
-# class Story_actionForm(forms.ModelForm):
+class Story_actionForm(forms.ModelForm):
 
-#     class Meta:
-#         model = Story_action
-#         labels={
-#         'msg_example':'Пример сообщения',
-#         'validation_option':'Уточнение перед действием',
-#         'action_name':'Название вызываемой функции',
-#         'action_answer':'Ответ с переменными',
-#         'transcript':"Краткий смысл функции на русском",
-#         'desc':'Описание'
-#         }
-#         fields = ('msg_example', 'validation_option',"action_name","action_answer","transcript","desc")
+    class Meta:
+        model = Story_action
+        widgets = { 
+        'action_name': forms.TextInput(attrs={'size':38}),
+        'transcript': forms.TextInput(attrs={'size':38}),
+        'msg_example': forms.Textarea(attrs={'rows': 2, 'cols': 40}),
+        'action_answer':forms.Textarea(attrs={'rows': 2, 'cols': 40}),
+        'desc':forms.Textarea(attrs={'rows': 2, 'cols': 40})
+        }
+
+        labels={
+        'msg_example':'Пример сообщения',
+        'validation_option':'Уточнение перед действием',
+        'action_name':'Название вызываемой функции (python)',
+        'action_answer':'Ответ с переменными',
+        'transcript':"Краткий смысл функции на русском",
+        'desc':'Описание'
+        }
+        fields = ('msg_example', 'validation_option',"action_name","action_answer","transcript","desc")
--- panel/views.py
+++ panel/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render,redirect,get_object_or_404
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from assistant_telegram1.models import *
 from django.core.urlresolvers import reverse_lazy
-from .forms import IntentForm,Story_msgForm
+from .forms import IntentForm,Story_msgForm,Story_actionForm
 # Create your views here.
 class Index_intentsView(generic.ListView):
 	template_name="intent/index_intents.html"
@@ -13,17 +13,55 @@ class Index_intentsView(generic.ListView):
 		return Intent.objects.all()
 
 # class Detail_intentView(generic.DetailView):
-def detail_intent(request,intent_name):
-    intent=get_object_or_404(Intent,name=intent_name)
-    # if intent.intent_type=="msg":
-    form = Story_msgForm()
-    return render(request,"intent/detail_intent.html",{"intent":intent,"form":form})
+def detail_intent(request,pk):
+    intent=get_object_or_404(Intent,name=pk)
+    if request.method == "POST":
+        if intent.intent_type=="msg":
+            if hasattr(intent,"story_msg"):
+                form = Story_msgForm(request.POST, instance=intent.story_msg)
+            else:
+                form = Story_msgForm(request.POST)
+            if form.is_valid():
+                if hasattr(intent,"story_action"):
+                    intent.story_action.delete()
+                    story_msg=form.save(commit=False)
+                    story_msg.intent=intent
+                    story_msg.save()
+                return redirect('panel:index_intents')
+        else:
+            if intent.intent_type=="action":
+                if hasattr(intent,"story_action"):
+                    form = Story_actionForm(request.POST, instance=intent.story_action)
+                else:
+                    form = Story_actionForm(request.POST)
+                if form.is_valid():
+                    if hasattr(intent,"story_msg"):
+                        intent.story_msg.delete()
+                        story_action=form.save(commit=False)
+                        story_action.intent=intent
+                        story_action.save()
+                    return redirect('panel:index_intents')
+    else:
+        if intent.intent_type=="msg":
+            if hasattr(intent,"story_msg"):
+                form = Story_msgForm(instance=intent.story_msg)
+            else:
+                form = Story_msgForm()
+        else:
+            if hasattr(intent,"story_action"):
+                form = Story_actionForm(instance=intent.story_action)
+            else:
+                form = Story_actionForm()
+        return render(request,"intent/detail_intent.html",{"intent":intent,"form":form})
 
 def intent_new(request):
     if request.method == "POST":
         form = IntentForm(request.POST)
+        updated_data = request.POST.copy()
+        updated_data.update({'name': form.data['name'].lower()}) 
+        form = IntentForm(data=updated_data) 
         if form.is_valid():
-            intent = form.save()
+            intent = form.save(commit=False)
             intent.save()
             return redirect('panel:detail_intent', pk=intent.name)
     else:
@@ -35,7 +73,7 @@ def intent_edit(request, pk):
     if request.method == "POST":
         form = IntentForm(request.POST, instance=intent)
         if form.is_valid():
-            intent = form.save()
+            intent = form.save(commit=False)
             intent.save()
             return redirect('panel:detail_intent', pk=intent.name)
     else:
